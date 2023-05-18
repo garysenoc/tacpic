5fd7d5b6d396e9cdd6e6af028c27ed7ac8e9bd8b
garysenoc <garysenoc@gmail.com>
2023-05-18 14:17:30 +0800

    fix: size

--- src/canvas/constants/defaults.ts
+++ src/canvas/constants/defaults.ts
@@ -32,10 +32,10 @@ export const gridOption = {
 };
 
 export const workareaOption: Partial<WorkareaObject> = {
-	width: 600,
-	height: 400,
-	workareaWidth: 600,
-	workareaHeight: 400,
+	width: 2480,
+	height: 3508,
+	workareaWidth: 2480,
+	workareaHeight: 3508,
 	lockScalingX: true,
 	lockScalingY: true,
 	scaleX: 1,
